98477d5567d5e242618f5980e647547a80cc4f94
tinmanCtr <ryo.prfm27@gmail.com>
2021-06-01 00:03:14 +0900

    feat: favorite usecase

---
+++ src/internal/usecase/favorite.go
@@ -0,0 +1,38 @@
+package usecase
+
+import (
+	"artics-api/src/internal/domain"
+	"artics-api/src/internal/domain/favorite"
+	"artics-api/src/internal/domain/user"
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type FavoriteUsecase interface {
+	Create(ctx context.Context, contentID string) error
+}
+
+type favoriteUsecase struct {
+	userService     user.UserService
+	favoriteService favorite.FavoriteService
+}
+
+func NewFavoriteUsecase(us user.UserService, fs favorite.FavoriteService) FavoriteUsecase {
+	return &favoriteUsecase{us, fs}
+}
+
+func (fu *favoriteUsecase) Create(ctx context.Context, contentID string) error {
+	u, err := fu.userService.Auth(ctx)
+	if err != nil {
+		return domain.Unauthorized.New(err)
+	}
+
+	f := &favorite.Favorite{
+		ID:        uuid.New().String(),
+		UserID:    u.ID,
+		ContentID: contentID,
+	}
+
+	return fu.favoriteService.Create(ctx, f)
+}
